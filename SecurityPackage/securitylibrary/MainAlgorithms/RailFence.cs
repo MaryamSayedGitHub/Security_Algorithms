eb9eb21ecff60a545edf958a7fb29c67f3531ee7
Maryam Sayed Ahmed <132791426+MaryamSayedGitHub@users.noreply.github.com>
2025-03-06 17:37:24 +0200

    Update RailFence.cs

--- SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,18 +9,103 @@ namespace SecurityLibrary
     public class RailFence : ICryptographicTechnique<string, int>
     {
         public int Analyse(string plainText, string cipherText)
+
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            cipherText = cipherText.Replace("x", "");
+
+            int n = 2;
+
+            while (true)
+            {
+                string encrypted = Encrypt(plainText, n);
+                if (string.Equals(encrypted, cipherText)) break;
+                else
+                    n++;
+            }
+
+            return n;
         }
 
-        public string Decrypt(string cipherText, int key)
+        public string Decrypt(string cipherText, int n)
         {
-            throw new NotImplementedException();
+            int c = Convert.ToInt32(Math.Ceiling((double)cipherText.Length / (double)n));
+
+            char[,] matrix = new char[n, c];
+
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+
+                    if (counter < cipherText.Length)
+                    {
+
+                        matrix[i, j] = cipherText[counter++];
+                    }
+
+                    else break;
+
+                }
+            }
+
+            string decryption = null;
+
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[j, i] != '\0')
+                        decryption += matrix[j, i];
+                }
+            }
+
+            return decryption;
         }
 
-        public string Encrypt(string plainText, int key)
+        public string Encrypt(string plainText, int n)
         {
-            throw new NotImplementedException();
+            plainText = plainText.Trim();
+            int c = Convert.ToInt32(Math.Ceiling((double)plainText.Length / (double)n));
+
+            Console.WriteLine(c);
+
+            char[,] matrix = new char[n, c];
+            Console.WriteLine(matrix.Length);
+            int counter = 0;
+
+
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    if (counter < plainText.Length)
+                    {
+
+                        matrix[j, i] = plainText[counter++];
+                    }
+
+
+                }
+            }
+            string encryption = null;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    if (matrix[i, j] != '\0')
+                        encryption += matrix[i, j];
+                }
+            }
+
+            return encryption.Trim();
         }
+        
+
     }
 }
